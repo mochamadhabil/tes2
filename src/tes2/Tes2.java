4cc0cefb99b3233b08ba0602d5bdba54aff38c6a
abil <abil@LAPTOP-D1KLFAE4>
2023-11-17 01:14:08 +0700

    First Habil

---
+++ src/tes2/Tes2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tes2;
+
+/**
+ *
+ * @author abil
+ */
+public class Tes2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
